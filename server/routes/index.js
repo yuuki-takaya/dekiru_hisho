9293162145f441049484251928158741e65132c0
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2017-12-11 15:04:32 +0900

    とりあえずコンソールでとってくることはできた（新しいtoken取得等の実装ができているかは不明）

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,170 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var request = require('request');
+
+
+
+router.get('/token',function(req,res,next){
+  console.log(req.query.code);
+  
+  var options = {
+    url: 'https://localhost:3000/newtoken?code='+req.query.code,
+    json: true
+  };
+
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //getリクエスト成功時の処理
+    } else {
+      console.log('error: '+ response.statusCode);
+    }
+  });
+});
+
+router.get('/newtoken',function(req,res,next){
+  getNewToken(oauth2Client,req.query.code,listEvents);
+
+});
+
+
+/* GET home page. */
+router.get('/auth', function(req, res, next) {
+  // If modifying these scopes, delete your previously saved credentials
+  // at ~/.credentials/calendar-nodejs-quickstart.json
+  var SCOPES = ['https://www.googleapis.com/auth/calendar'];
+  var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+  var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
+  console.log(TOKEN_PATH);
+  
+
+  // Load client secrets from a local file.
+  fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Calendar API.
+    authorize(JSON.parse(content), listEvents);
+  });
+
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  // var redirectUrl = credentials.installed.redirect_uris[0];
+  var redirectUrl = 'https://localhost:3000/token';
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      res.redirect('/newtoken');
+    } else {
+      console.log("もうtoken持ってる\n");
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+function getNewToken(oauth2Client,code,callback) {
+  console.log("in getNewToken\n")
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  res.redirect(authUrl);
+
+  oauth2Client.getToken(code, function(err, token) {
+    if (err) {
+      console.log('Error while trying to retrieve access token', err);
+      return;
+    }
+    oauth2Client.credentials = token;
+    storeToken(token);
+    callback(oauth2Client);
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listEvents(auth) {
+  var calendar = google.calendar('v3');
+  console.log("authの中身だよ〜\n");
+  console.log(auth);
+  calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var events = response.items;
+    console.log(response.items);
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+    } else {
+      console.log('Upcoming 10 events:');
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+        console.log('%s - %s', start, event.summary);
+      }
+    }
+  });
+}
+
+function enterCode(oauth2Client,code) {
+  oauth2Client.getToken(code, function(err, token) {
+    if (err) {
+      console.log('Error while trying to retrieve access token', err);
+      return;
+    }
+    oauth2Client.credentials = token;
+    storeToken(token);
+    callback(oauth2Client);
+  });
+}
+
+});
+
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
